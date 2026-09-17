fc62cf00fa255d8522437bc1f0bbdf22e997b613
Umar <heyumar@gmail.com>
2026-09-17 19:14:10 +0100

    Show decimal points in place value grids

--- content/place-value/index.php
+++ content/place-value/index.php
@@ -27,7 +27,7 @@ $pageTitle = 'Place value';
         type="font/woff2"
         crossorigin
     >
-    <link rel="stylesheet" href="../../assets/css/app.css?v=13">
+    <link rel="stylesheet" href="../../assets/css/app.css?v=14">
 </head>
 
 <body
