f09231b38a4972aed142325433be497196e229b7
Umar <heyumar@gmail.com>
2026-09-17 20:05:03 +0100

    Compact mobile resume notice

--- content/place-value/index.php
+++ content/place-value/index.php
@@ -27,7 +27,7 @@ $pageTitle = 'Place value';
         type="font/woff2"
         crossorigin
     >
-    <link rel="stylesheet" href="../../assets/css/app.css?v=16">
+    <link rel="stylesheet" href="../../assets/css/app.css?v=17">
 </head>
 
 <body
