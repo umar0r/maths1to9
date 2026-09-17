35a0cbf032f0b9e5eafcaa7dfa37ff9756e8792f
Umar <heyumar@gmail.com>
2026-09-17 11:38:30 +0100

    Center decimal ordering interaction

--- content/place-value/index.php
+++ content/place-value/index.php
@@ -27,7 +27,7 @@ $pageTitle = 'Place value';
         type="font/woff2"
         crossorigin
     >
-    <link rel="stylesheet" href="../../assets/css/app.css?v=4">
+    <link rel="stylesheet" href="../../assets/css/app.css?v=5">
 </head>
 
 <body
